657264665c336982cce4f844e62739e8b6dce8c0
Elizabeth Joseph <102416858+ElizabethMawutin@users.noreply.github.com>
2022-08-19 18:46:38 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
-// window.alert('Welcome to the Registration Page');
+window.alert('Welcome to the Registration Page');
 // console.log(document.querySelectorAll('.btn'));
 
 // const btn = document.querySelector('.btn');
 // btn.style.background = 'red';
 
-//const sb = document.getElementsByClassName('sb');
+const sb = document.querySelector('.sb');
 
-//sb.addEventListener('click' (e) => {
-   // e.preventDefault();
-    //document.querySelector('.container').style.backgroundcolor = 'red';
-//});
+sb.addEventListener('click', (e) => {
+    e.preventDefault();
+    document.querySelector('.container').style.background = 'white';
+});
 
 document.querySelector(".sb").style.backgroundColor= "red";
+
 
