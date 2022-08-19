d1cf75a59942c2d2410a587da28b09ba76f53842
Elizabeth Joseph <102416858+ElizabethMawutin@users.noreply.github.com>
2022-08-19 18:15:31 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+// window.alert('Welcome to the Registration Page');
+// console.log(document.querySelectorAll('.btn'));
+
+// const btn = document.querySelector('.btn');
+// btn.style.background = 'red';
+
+//const sb = document.getElementsByClassName('sb');
+
+//sb.addEventListener('click' (e) => {
+   // e.preventDefault();
+    //document.querySelector('.container').style.backgroundcolor = 'red';
+//});
+
+document.querySelector(".sb").style.backgroundColor= "red";
+
